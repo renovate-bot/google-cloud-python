ae965c7434abe2ac59dea4cd66fca61e4790c636
Chalmer Lowe <chalmer.lowe@gmail.com>
2026-02-23 08:48:01 -0500

    Removes reference to pkg-resources.
    Removes reference to pkg-resources.

--- packages/google-cloud-logging/setup.py
+++ packages/google-cloud-logging/setup.py
@@ -53,7 +53,6 @@ dependencies = [
     "proto-plus >= 1.22.2, <2.0.0; python_version>='3.11'",
     "proto-plus >= 1.25.0, <2.0.0; python_version>='3.13'",
     "protobuf>=3.20.2,<7.0.0,!=4.21.0,!=4.21.1,!=4.21.2,!=4.21.3,!=4.21.4,!=4.21.5",
-    "pkg-resources",
 ]
 url = "https://github.com/googleapis/python-logging"
 
